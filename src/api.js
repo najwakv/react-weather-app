e5348c2620f2d0134b31044cb030522c756e6af1
najwa <najwa.kv20@gmail.com>
2023-07-26 13:49:35 +0530

    Weather app completed

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const gioApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '526a591e05msh9e2b605a989bfafp143fc6jsn3d1db4e8390d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "0c82c5b159f59f04d34b41c541d4575a"
---
+++ src/components/search/search.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, gioApiOptions } from "../../api";
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000&namePrefix=${inputValue}`,
+      gioApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+export default Search;
